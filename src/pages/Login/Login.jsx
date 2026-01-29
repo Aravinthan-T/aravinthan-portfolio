e33b94c6906a66eda8e45c8482ccdf83aed617b1
Aravinthan <aravinthan.t.2204@gmail.com>
2026-01-29 21:59:08 +0530

    Adding Login function using JWT

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -31,7 +31,7 @@ export default function Login() {
                   );
 
                   login(Array.from(credentialResponse.credential));
-                  navigate("/landingpage");
+                  navigate("/");
                 }}
                 onError={() => console.log("Login Failed")}
               />
